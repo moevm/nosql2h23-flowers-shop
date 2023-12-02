f73ed13f6f5d5dd2e27e0b9e211e3bea27d3643e
galinaOkhotnikova <71720610+galinaOkhotnikova@users.noreply.github.com>
2023-12-02 12:58:17 +0300

    add main page with fake data

---
+++ client/README.md
@@ -0,0 +1,4 @@
+# Instruction
+
+1. run __npm i__
+2. run npm __run dev__
---
+++ client/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+    <Navbar />
+    <Header />
+    <Slider />
+    <Reviews />
+    <Footer />
+</template>
+
+<script setup>
+import Navbar from './components/Navbar.vue'
+import Header from './components/Header.vue'
+import Slider from './components/Slider.vue'
+import Reviews from './components/Reviews.vue'
+import Footer from './components/Footer.vue'
+</script>
+
+<style scoped>
+</style>
