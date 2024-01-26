867beadacd1d74c84742060df25eee1c8561c7e7
oksi-oksik <oksi-oksik13@mail.ru>
2024-01-27 01:52:53 +0300

    add queries for warehouse

--- backend/app.py
+++ backend/app.py
@@ -23,5 +23,34 @@ def get_catalog():
     return(json.dumps(data, default=str, ensure_ascii=False).encode('utf8'))
 
 
+@app.route("/addProduct", methods=["GET", "POST"])
+def addProduct():
+    response = {'status':'success'}
+    data = request.get_json()
+    add_query = """
+    MATCH (n:User {id: $user_id, role: 'Продавец'})
+    CREATE (n)-[:IS_SELLER_OF]->(product:Product {id: toInteger($product_id), name: $name, image: $image,\
+        price: toFloat($price), shelf_life: Date($shelf_life), description: $description, amount: toInteger($amount)});
+    """
+    map={"user_id": data.get('user_id'), "product_id": data.get('product_id'), "name": data.get('name'), "image": data.get('image'),\
+        "price": data.get('price'), "shelf_life": data.get('shelf_life'), "description": data.get('dedscription'), "amount": data.get('amount')}
+    try:
+        session.run(add_query, map)
+    except Exception as e:
+        response['status'] = 'failed'
+        response['message'] = str(e)
+    return(json.dumps(response))
+
+@app.route("/warehouse", methods=["GET", "POST"])     
+def get_warehouse():
+    data = request.get_json()   
+    query="""MATCH (n:User {id: $user_id})-[:IS_SELLER_OF]->(m) RETURN m"""
+    map={"user_id": data.user_id}
+    results=session.run(query, map)
+    response=results.data() 
+    return(json.dumps(response, default=str, ensure_ascii=False).encode('utf8'))
+
+
+
 if __name__ == '__main__':
     app.run(debug=True, port=5050)
